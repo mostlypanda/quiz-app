27faf0c5d9289a1998bba6b16561cb2b81460d16
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-14 17:04:23 +0530

    functionality installed

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = module.exports = express();
+app.mongoose = require("mongoose");
+
+var config = require("./config.js")(app, express);
+
+var models = {};
+models.Question = require("./models/question")(app.mongoose).model;
+models.Score = require("./models/score")(app.mongoose).model;
+
+require("./routes/routes")(app, models);
+
+app.listen(3000,()=>{
+    console.log('app is live at 3000');
+});
