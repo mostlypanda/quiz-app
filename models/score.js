b16ad8c58d51c745720353b0e111d0b6b443e9ac
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-14 17:11:54 +0530

    score schema done

---
+++ models/score.js
@@ -0,0 +1,15 @@
+module.exports = function(mongoose) {
+	var Schema = mongoose.Schema;
+	var ObjectId = Schema.ObjectId;
+
+	var schema = new Schema({
+		username: String,
+		score: Number,
+		date: {type: Date, default: Date.now},
+		country: String
+	});
+
+	this.model = mongoose.model('Score', schema);
+
+	return this;
+};  
