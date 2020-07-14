8758e4de7856f435562dd98061934b9eb4f4bcad
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-14 17:11:19 +0530

    question schema done

---
+++ models/question.js
@@ -0,0 +1,16 @@
+module.exports = function(mongoose) {
+	var Schema = mongoose.Schema;
+	var ObjectId = Schema.ObjectId;
+
+	var schema = new Schema({
+		title: String,
+		answers: {
+			correct: String,
+			incorrect: [String] },
+		entered: {type: Date, default: Date.now}
+	});
+
+	this.model = mongoose.model('Question', schema);
+
+	return this;
+};
